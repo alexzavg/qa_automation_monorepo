13568aec581418e2797f2b88e02ad3463241198e
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2025-06-02 19:34:29 +0300

    1

--- playwright/tests/apis/soap.spec.ts
+++ playwright/tests/apis/soap.spec.ts
@@ -5,6 +5,7 @@ import { additionData } from "../../TestData/soapAPI.data"
 test.describe('SOAP Example', () => {
 
   test(`Calculator "Add" operation`, async ({ pageManager }) => {
+    console.log(`Addition data: ${JSON.stringify(additionData)}`)
     const result = await pageManager.soapExampleApi.add(additionData)
     expect(result).toBe(additionData.a + additionData.b)
   })
