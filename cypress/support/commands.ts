f09289a2f502851fb20d4fb76138b1d7aa3e9849
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2025-06-05 12:19:43 +0300

    1

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,15 +1,1 @@
 /// <reference types="cypress" />
-
-// cypress/support/commands.ts
-import pageManager from './pageManager'
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      pages: typeof pageManager
-    }
-  }
-}
-
-// Add pages to cy.pages
-Cypress.Commands.add('pages', { prevSubject: false }, () => pageManager)
